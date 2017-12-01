fe3b47703ec49b12838e23e3e7712b452938a0da
Imperial7 <bobo201345@gmail.com>
2017-12-01 12:00:18 +0300

    Game

--- main.js
+++ main.js
@@ -4,29 +4,57 @@ var map_width = canvas.width = canvas.scrollWidth = 1280;
 var map_height = canvas.height = canvas.scrollHeight = 640;
 var LastFrame = 0;
 var FPS = 0;
+var players = [];
+var player_sprite;
 
 function Awake()
 {
+    player_sprite = new Sprite("Images/g4502.png",{
+        width: 128,
+        height: 128
+    });
     Start();
 }
 
 function Start()
 {
+    var new_player = new Player(map_width/2,map_height/2);
+    players.push(new_player);
     Update();
 }
 
 function Update()
 {
     draw_background("white");
+    for(var i = 0; i < players.length; i++)
+    {
+        players[i].update();
+        players[i].draw();
+    }
     requestAnimationFrame(Update);
+}
+
+function Player(x,y)
+{
+    this.x = x;
+    this.y = y;
+    this.sprite = player_sprite;
+    this.update = function()
+    {
+
+    }
+    this.draw = function()
+    {
+        this.sprite.draw(this.x-this.sprite.width/2,this.y-this.sprite.height/2);
+    }
 }
 
 function Sprite(source,options)
 {
     this.source = new Image();
     this.source.src = source;
-    this.width = this.source.width || options.width || 0;
-    this.height = this.source.height || options.height || 0;
+    this.width = options.width || 0;
+    this.height = options.height || 0;
     this.image_index = options.image_index || 0;
     this.image_number = options.image_number || 1;
     this.play = options.play || false;
